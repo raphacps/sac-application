56393ad4677214e8ad9933701809fe6840444336
Raphael Soares <raphacps@gmail.com>
2017-10-22 21:25:55 -0200

    criar testes de integracao com banco de dados

--- sac-query/src/main/java/com/olx/sac/entity/Issue.java
+++ sac-query/src/main/java/com/olx/sac/entity/Issue.java
@@ -1,7 +1,12 @@
 package com.olx.sac.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Objects;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.Entity;
@@ -12,12 +17,15 @@ import javax.validation.constraints.NotNull;
 import java.util.Calendar;
 import java.util.Date;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Entity
 public class Issue {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private long id;
 
     @NotBlank
@@ -36,7 +44,27 @@ public class Issue {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "America/Sao_Paulo")
     private Date createdAt;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     public int getDay() {
-        return createdAt.getDay();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(createdAt);
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Issue issue = (Issue) o;
+        return Objects.equal(description, issue.description) &&
+                Objects.equal(reason, issue.reason) &&
+                Objects.equal(state, issue.state) &&
+                Objects.equal(type, issue.type) &&
+                Objects.equal(createdAt, issue.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(description, reason, state, type, createdAt);
     }
 }
--- sac-query/src/test/java/com/olx/sac/service/AttendanceServiceIT.java
+++ sac-query/src/test/java/com/olx/sac/service/AttendanceServiceIT.java
@@ -3,24 +3,31 @@ package com.olx.sac.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.olx.sac.entity.Issue;
+import com.olx.sac.entity.IssueRepository;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.util.List;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 /**
  * Created by raphael on 18/10/17.
  */
 @RunWith(SpringRunner.class)
-public class AttendanceServiceTest {
+@DataJpaTest
+public class AttendanceServiceIT {
 
     @Value(value = "classpath:desorderedIssueList.json")
     private Resource desorderedIssueList;
@@ -31,6 +38,12 @@ public class AttendanceServiceTest {
     private List<Issue> desorderedIssues;
     private List<Issue> orderedIssues;
 
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private IssueRepository issueRepository;
+
     @Before
     public void setUp() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -40,7 +53,11 @@ public class AttendanceServiceTest {
 
     @Test
     public void findAllIssues() throws Exception {
-
+        desorderedIssues.forEach(issue -> testEntityManager.persist(issue));
+        List<Issue> issueListTest = issueRepository.findAllOrderByCreatedAt();
+        for(int i = 0; i< orderedIssues.size(); i++) {
+            assertThat(issueListTest.get(i), is(equalTo(orderedIssues.get(i))));
+        }
     }
 
 }
